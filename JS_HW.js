212eb1c9c916585d0dd204b6048b5198dc3019fa
kremnyova <99237002+kremnyova@users.noreply.github.com>
2022-06-22 14:19:31 +0300

    Update JS_HW.js

--- JS_HW.js
+++ JS_HW.js
@@ -1,9 +1,7 @@
 let age_2 = 18
 let age_3 = 60
-var a = prompt('Your age is:')
 let checkAge = function(age_1) {
-    age_1 = Number(age_1)
-    if (isNaN(age_1)) {
+    if (age_1 && isNaN(age_1)) {
         console.log('Кидается ошибка') }
     else if (age_1 < age_2) {
         console.log("You don’t have access cause your age is " + age_1 + " It’s less then ")
@@ -19,4 +17,4 @@ let checkAge = function(age_1) {
     }
 }
 
-checkAge(a)
+checkAge(+prompt('Enter your age', ''))
